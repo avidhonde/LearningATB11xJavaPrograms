fe89d8d58bbd07deb49b9c160f0830c56912dd3c
Avinash <avidhonde53@gmail.com>
2025-03-12 07:05:29 +0530

    basic exercise java

---
+++ src/ex_01_Java_Basic/Task11March001.java
@@ -0,0 +1,19 @@
+package ex_01_Java_Basic;
+
+public class Task11March001 {
+    public static void main(String[] args) {
+        int a = 9;
+        System.out.printf("%d", a * 1 );
+        System.out.printf("%d", a * 2 );
+        System.out.printf("%d", a * 3 );
+        System.out.printf("%d", a * 4 );
+        System.out.printf("%d", a * 5 );
+        System.out.printf("%d", a * 6 );
+        System.out.printf("%d", a * 7 );
+        System.out.printf("%d", a * 8 );
+        System.out.printf("%d", a * 9 );
+        System.out.printf("%d", a * 10 );
+
+
+    }
+}
