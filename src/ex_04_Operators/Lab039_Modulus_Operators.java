898e209c726eeb7e8984d9e11b6f4bbc8ab6420a
Avinash <avidhonde53@gmail.com>
2025-03-16 21:25:15 +0530

    all exercises

---
+++ src/ex_04_Operators/Lab039_Modulus_Operators.java
@@ -0,0 +1,22 @@
+package ex_04_Operators;
+
+public class Lab039_Modulus_Operators {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        System.out.println(a%b);
+        // Mod -> Remainder,
+//        10 | 20 |  2 - Quoinet
+//           | 20 |
+//           -------
+//             0 - Remainder
+//           ----
+        System.out.println(13%7);
+        //        7 | 13 |  1 - Quoinet
+        //           | 7 |
+        //           -------
+        //             6 - Remainder
+        //           ----
+//      Output = 0, 6
+    }
+}
