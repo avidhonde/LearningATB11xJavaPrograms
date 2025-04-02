8cc0093e5ec1accc7988be4de04bff894c17e297
Avinash <avidhonde53@gmail.com>
2025-04-02 09:03:52 +0530

    practice task

---
+++ src/ex_10_For_Loop/Lab_103_For_If_LOOP.java
@@ -0,0 +1,13 @@
+package ex_10_For_Loop;
+
+public class Lab_103_For_If_LOOP {
+    public static void main(String[] args) {
+        for (int i=0; i<=25; i++){
+        if (i >18) {
+            System.out.println("entry for club");
+        } else {
+            System.out.println("NO Entry for club");
+        }
+        }
+    }
+}
