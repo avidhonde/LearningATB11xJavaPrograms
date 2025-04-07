8443d16176961011f2fb8ef61c89b0006b636f50
Avinash <avidhonde53@gmail.com>
2025-04-07 09:39:20 +0530

    practice task

---
+++ src/ex_12_Dowhile/Lab127_DoWhile_Real.java
@@ -0,0 +1,15 @@
+package ex_12_Dowhile;
+
+public class Lab127_DoWhile_Real {
+    public static void main(String[] args) {
+//        web automation
+        int number = 0;
+//        0 it can be 0-10 - elments or students
+        do {
+            System.out.println(number);
+//            call function(number);
+//            close the excel file open the excel file
+            number++;
+        }while (number>0);
+    }
+}
---
+++ src/ex_13_functions/Lab128_Functions.java
@@ -0,0 +1,13 @@
+package ex_13_functions;
+import java.util.*;
+
+public class Lab128_Functions {
+    public static void main(String[] args) {
+//        Build - in
+//        Created by java guys
+        int result = Math.max(3,4);
+        int min = Math.min(3,4);
+        System.out.println(result);
+        System.out.println(min);
+    }
+}
