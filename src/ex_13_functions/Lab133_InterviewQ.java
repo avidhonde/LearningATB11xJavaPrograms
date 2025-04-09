785e8beb6284ae9f5656e1ff1d682f30da5f3490
Avinash <avidhonde53@gmail.com>
2025-04-09 08:57:09 +0530

    practice task

---
+++ src/ex_13_functions/Lab133_InterviewQ.java
@@ -0,0 +1,23 @@
+package ex_13_functions;
+
+public class Lab133_InterviewQ {
+    public static void main(String[] args) {
+        main(3.14f);
+    }
+
+    public static void main(String args) {
+
+    }
+
+    public static int main(int args) {
+        return 10;
+    }
+
+    public static boolean main(boolean args) {
+        return true;
+    }
+
+    public static float main(float args) {
+        return 10;
+    }
+}
